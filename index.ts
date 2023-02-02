e2975b2402067d94e441470560bb32be5ff7352c
Kamil Rutyna <kamil.p.rutyna@gmail.com>
2023-02-02 13:30:11 +0100

    Third changes

--- index.ts
+++ index.ts
@@ -1,8 +1,14 @@
 import React from 'react';
+import { useState } from 'react';
 
-const func = (): boolean => {
-	console.log("Hello World");
-	return true;
+const func = (): ReactComponent => {
+const [state, setState] = useState(true);
+	const text = "Hello World";
+	return(
+	<div>
+	<h1>{text}</h1>
+	</div>
+	)
 }
 
 export default func;
